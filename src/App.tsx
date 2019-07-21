a54423e6234d75c06cf4584264ef34efb17a8d3c
Roberto Albanesi <beto0607@gmail.comn>
2019-07-21 16:09:17 +0200

    Started FormSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import Layout from './layout/Layout';
 import 'bootstrap/dist/css/bootstrap.css';
+import Layout from './layout/Layout';
+import FormSection from './components/FormSection/FormSection';
 
 const App: React.FC = () => (
   <Layout>
+    <FormSection />
     <div>Wacala</div>
   </Layout>
 );
