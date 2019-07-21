f9c5d1b1aa54c72b7aae91fc97bdce067a2f4ed9
Roberto Albanesi <beto0607@gmail.comn>
2019-07-21 17:07:13 +0200

    Finished FormSection structure

--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { Form } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Form, FormControlProps, Button } from 'react-bootstrap';
+import styles from './form-section.module.scss';
+
+
 const FormSection: React.FC = () => {
+    const [text, setText] = useState('');
+    const handleSumbit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(text);
+    };
     return (
-        <div>
-            <Form>
+        <div className={styles['form-section']}>
+            <Form onSubmit={handleSumbit}>
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-    </Form.Text>
+                    <Form.Label>Search for a game</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter game type"
+                        onChange={(event: React.FormEvent<FormControlProps>) => { setText(event.currentTarget.value || '') }}
+                    />
                 </Form.Group>
+                <Button variant="primary" type="submit" block={true} disabled={!text}>
+                    Search
+                </Button>
             </Form>
 
-        </div>);
+        </div>
+    );
 };
 
 export default FormSection;
