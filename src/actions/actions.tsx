48371d8429109482d579033b872209feafde1c32
Roberto Albanesi <beto0607@gmail.comn>
2019-07-21 23:27:54 +0200

    Added base for action getGame

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -1,5 +1,7 @@
 import { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from 'redux';
+import { GAME_SCHEDULE_LOADED, GAME_RESPONSE_LOADED } from '../constants/action-types';
+
 // import * as constants from '../constants/action-types';
 
 // Typescripts INTERFACES
@@ -8,15 +10,28 @@ import { AnyAction } from 'redux';
 
 // Action Creators
 
-export const getGameSchedule = (text: string): ThunkAction<Promise<void>, {}, {}, AnyAction> => {
+export const getGameSchedule = (gameType: string): ThunkAction<Promise<void>, {}, {}, AnyAction> => {
     return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
-        return fetch(`http://localhost:3001/api/products/${text}`)
+        return fetch(`http://localhost:3001/api/products/${gameType}`)
             .then(response => response.json())
             .then(json => {
-                dispatch({ type: "GAME_SCHEDULE_LOADED", payload: { text: text, game_schedule: json } });
+                dispatch({ type: GAME_SCHEDULE_LOADED, payload: { text: gameType, gameSchedule: json } });
             }).catch((err) => {
                 console.error(err);
-                dispatch({ type: "GAME_SCHEDULE_LOADED", payload: { text: text, game_schedule: {} } });
+                dispatch({ type: GAME_SCHEDULE_LOADED, payload: { text: gameType, gameSchedule: {} } });
+            });
+    }
+}
+
+export const getGame = (gameId: string): ThunkAction<Promise<void>, {}, {}, AnyAction> => {
+    return async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
+        return fetch(`http://localhost:3001/api/games/${gameId}`)
+            .then(response => response.json())
+            .then(json => {
+                dispatch({ type: GAME_RESPONSE_LOADED, payload: { gameInfo: json } });
+            }).catch((err) => {
+                console.error(err);
+                dispatch({ type: GAME_RESPONSE_LOADED, payload: { gameInfo: {} } });
             });
     }
 }
